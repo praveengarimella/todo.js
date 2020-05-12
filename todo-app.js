d353994fd2183b3c12dae543a08c4841f3f34cf9
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2020-05-12 21:12:45 +0530

    Added Task

--- todo-app.js
+++ todo-app.js
@@ -1,14 +1,18 @@
 function Task(props) {
-  return <li>{props.name}, {new Date().toDateString()}</li>
+  return <li>{props.name}, <b>Due Date:</b> {new Date().toDateString()}</li>
 }
 
 class ToDo extends React.Component {
+
   render() {
+    const listItems = this.props.list.map((t) =>
+      <Task name={t.name} />
+    );
     return (
       <div>
         <h1>TO DO List</h1>
         <ol>
-          <Task name="Welcome Task" />
+          {listItems}
         </ol>
       </div>
 
@@ -16,4 +20,6 @@ class ToDo extends React.Component {
   }
 }
 
-ReactDOM.render(<ToDo />, document.getElementById('todo'));
+const tasksList = [{ name: "Welcome Task" }, { name: "Complete Todo in ReactJS" }]
+
+ReactDOM.render(<ToDo list={tasksList} />, document.getElementById('todo'));
