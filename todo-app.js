b7d899b26bd57598d9b08659b9e0a4f7efeb3329
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2020-05-13 10:36:54 +0530

    add task

--- todo-app.js
+++ todo-app.js
@@ -1,25 +1,80 @@
 function Task(props) {
-  return <li>{props.name}, <b>Due Date:</b> {new Date().toDateString()}</li>
+  return <li>{props.name}, {new Date().toLocaleDateString()}</li>
 }
 
 class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: props.list };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+
+  }
+
+
+  handleAddTask(task) {
+    this.state.list.push(task);
+    this.setState({ list: this.state.list });
+  }
 
   render() {
-    const listItems = this.props.list.map((t) =>
-      <Task name={t.name} />
-    );
+
     return (
       <div>
         <h1>TO DO List</h1>
-        <ol>
-          {listItems}
+        <ol>{
+          this.listItems = this.props.list.map((t) =>
+            <Task key={t.id} name={t.name} />
+          )}
         </ol>
+        <Form onAddTask={this.handleAddTask} />
+
       </div>
 
     );
   }
 }
 
-const tasksList = [{ name: "Welcome Task" }, { name: "Complete Todo in ReactJS" }]
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' };
 
-ReactDOM.render(<ToDo list={tasksList} />, document.getElementById('todo'));
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const tasksListItems = this.props.tasksListItems;
+    event.preventDefault();
+    const task = { id: Date.now(), name: this.state.value, dueDate: new Date() };
+    this.props.onAddTask(task);
+
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <input
+          type="submit"
+          value="Add Task" />
+
+      </form>
+
+    );
+  }
+
+}
+
+
+ReactDOM.render(<ToDo list={[]} />, document.getElementById('todo'));
