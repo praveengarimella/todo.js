3dcdfc8604f770a4f74c171940fc92210472bd66
jayasimha099 <jayasimha1495@gmail.com>
2020-05-19 00:04:47 +0530

    To-Do list with Js

---
+++ application.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/api/todo/", methods=["POST", "GET"])
+def index():
+    if request.method == "GET":
+        data = request.json
+        with open("task.json", "r") as read_file:
+            data = json.load(read_file)
+            return jsonify(data), 200
+    return "<h1>Bad request</h1>", 400
+
+
+@app.route("/api/update/", methods=["POST", "GET"])
+def update():
+    if request.method == "POST":
+        data = request.json
+        with open("task.json", "w") as write_file:
+            json.dump(data, write_file)
+
+    return "<h1>Bad Request</h1>", 400
+
+
+if __name__ == "_main_":
+    app.run()
--- todo-app.js
+++ todo-app.js
@@ -1,74 +1,141 @@
 let taskList = []
+var newList=[]
+var time_list = new Date().toJSON().slice(0, 10).replace(/-/g, "-");
+
+var check=[]
+
+var currDate = new Date();
+var date = currDate.getDate() + "/" + currDate.getMonth() + 1;
 
 class Task {
-    constructor(name, dueDate, isDone) {
+    constructor(name, currentDate, isDone, dueDate, taskId) {
+      if (typeof taskId === "undefined") {
         this.taskId = Date.now();
         this.name = name;
-        this.dueDate = dueDate;
+        this.currentDate = date;
         this.isDone = isDone;
+        this.dueDate = dueDate;
+      } else {
+        this.taskId = taskId;
+        this.name = name;
+        this.currentDate = currentDate;
+        this.isDone = isDone;
+        this.dueDate = dueDate;
+      }
     }
 
     toString() {
-        let htmlText = '<li class="task" ><div>'
-        htmlText += this.name
-        htmlText += ", " + this.dueDate.getDate() 
-                 + "/" + this.dueDate.getMonth();
-        htmlText += '<input type="checkbox" name="isDone" id="isDone">'
-        htmlText += '<button onclick="deleteTask(';
+        let htmlText = '<li class="task" ><div class ="eachTask">';
+        htmlText += this.name;
+        htmlText += ", " + this.currentDate;
+        htmlText += ", " + "Due date: " + this.dueDate;        
+        htmlText += '<button onclick="del_Task(';
         htmlText += this.taskId;
         htmlText += ')">Delete</button>';
-        htmlText += '</div></li>';
+        htmlText += "</div></li>";
         return htmlText;
+      }
     }
-}
 
 function render() {
-    const listUI = document.getElementById("todolist")
+    console.log("entered")
+    const listUI = document.getElementById("todolist");
     listUI.innerHTML = "";
-    if (taskList.length === 0) listUI.innerHTML = "No tasks todo :-)"
-    taskList.forEach((task) => {
-        listUI.innerHTML += task.toString();
-    })
-}
+    if (newList.length === 0) listUI.innerHTML = "No tasks scheduled :-)";
+    newList.forEach((task) => {
+      listUI.innerHTML += task.toString();
+    });
+  }
 
-function deleteTask(taskId) {
-    taskList = taskList.filter(
+function del_Task(taskId) {
+    newList = newList.filter(
         (t) => {
             if(t.taskId != taskId) 
             return t;
         }
     );
-    // call a web api to update the database on the server
-    
-    // update the DOM
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:5000/api/update/", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(newList));
     render()
-    console.log(taskList);
+    console.log(newList);
 }
 
 function createTask() {
     const taskName = document.getElementById("taskName").value;
-    addTask(new Task(taskName, new Date(), false));
-}
-
-function addTask(t) {
-    taskList.push(t)
-    // call a web api to update the database on the server
+    const dueDate = document.getElementById("dueDate").value;
+    let msg = document.getElementById("message");
+    console.log(taskName);
+  
+    if (taskName === "") {
+      document.querySelector(".message").innerHTML = "Enter Task name.";
+      return;
+    }
+  
+    if (dueDate < time_list) {
+      document.querySelector(".message").innerHTML = "Enter a valid date.";
+      return;
+    }
+  
+    adding_Task(new Task(taskName, new Date(), false, dueDate));
+  }
+function adding_Task(t) {
+    newList.push(t)
+    var request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:5000/api/update/", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(newList));
     render();
     console.log(taskList)
 }
+  
+  function deleteId(taskId) {
+    check = check.filter((t) => {
+      if (t != taskId) return t;
+    });
+  }
+  
+  function isChecked(id) {
+    if (check.length === 0) return 0;
+    for (let k = 0; k < check.length; k++) {
+      if (check[k] === id) {
+        return 1;
+      }
+    }
+  }
 
-function init() {
+  function fetchData() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:5000/api/todo/", true);
+    request.onload = function () {
+      if (request.status === 200) {
+        let data = JSON.parse(request.responseText);
+        taskList = data;
+        console.log(taskList);
+        console.log(taskList);
+        for (let i = 0; i < taskList.length; i++) {
+          task = new Task(
+            taskList[i].name,
+            taskList[i].currentDate,
+            taskList[i].isDone,
+            taskList[i].dueDate,
+            taskList[i].taskId
+          );
+          console.log(task);
+          newList.push(task);
+          render();
+        }
+        console.log(check);
+        return data;
+      }
+    };
+    request.send();
+  }
+function init() {   
     console.log("init called");
-
-    // call a web api to retrieve the task list
-    // write a function to send a api request
-    // get the JSON
-    // assign it to taskList
-    // render
-
-    task = new Task("welcome task", new Date("May 30, 2020"), false);
-    addTask(task);
-    console.log(task);
+    fetchData();
+    render();
 }
 
 init();
