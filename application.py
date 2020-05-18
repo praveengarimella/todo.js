4467109b5cd5180fba1ae8cd1029e48b90cc2e2c
athreshanju <athreshanju@msitprogram.net>
2020-05-18 22:12:58 +0530

    implemented reactJS

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/api/todo/", methods=["POST", "GET"])
+def index():
+    if request.method == "GET":
+        data = request.json
+        with open("task.json", "r") as read_file:
+            data = json.load(read_file)
+            
+            return jsonify(data), 200
+    return "<h1>Bad request</h1>", 400
+
+
+@app.route("/api/update/", methods=["POST", "GET"])
+def update():
+    if request.method == "POST":
+        data = request.json
+        
+
+        with open("task.json", "w") as write_file:
+            json.dump(data, write_file)
+
+    return "<h1>Updated</h1>", 200
+
+
+if __name__ == "__main__":
+    app.run()
